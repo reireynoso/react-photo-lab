93f67241a4a921aafead28fe017c2215eb54093b
reireynoso <reireynoso@gmail.com>
2020-02-02 16:55:36 -0500

    created photo card

---
+++ src/components/PhotoCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PhotoCard = () => {
+    return (
+        <div class="ui centered card">
+        <div class="image">
+            <img src="/images/avatar2/large/elyse.png"/>
+        </div>
+        <div class="content">
+            <a class="header">Elyse</a>
+        </div>
+        </div>
+    )
+}
+
+export default PhotoCard
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class PhotoContainer extends Component {
   render() {
     return (
-      <div className="ui container"> textInComponent </div>
+      <div className="ui container">
+          
+      </div>
     );
   }
 }
